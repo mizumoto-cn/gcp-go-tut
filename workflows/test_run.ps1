da6d9d45e64fddcd80890641b2cd1f85e39a0c49
Mizumoto <mizumotokunn@gmail.com>
2022-10-12 13:30:18 +0900

    updated_test_run

--- workflows/test_run.ps1
+++ workflows/test_run.ps1
@@ -1,7 +1,19 @@
-$PROJECT_ID = $args[0]
-$WORKFLOW = $args[1]
+$TRIGGER_SA = $args[0]
+$PROJECT_ID = $args[1]
+
+$WORKFLOW = "myEventWorkflow"
+
+.\0_setup.ps1 $TRIGGER_SA $PROJECT_ID
+
+.\1_create_workflow.ps1 $TRIGGER_SA $PROJECT_ID $WORKFLOW
 
 
 gsutil cp test.csv gs://${PROJECT_ID}-bucket/test.csv
 
-gcloud workflows executions list ${WORKFLOW} --limit=2 --location=asia-northeast1
+# wait 5 seconds
+Start-Sleep -s 10
+
+gcloud workflows executions list ${WORKFLOW} --limit=5 --location=asia-northeast1
+
+echo "After you get the execution ID, run the following command to get the execution status:"
+echo "gcloud workflows executions describe <execution_id> --location=asia-northeast1"
